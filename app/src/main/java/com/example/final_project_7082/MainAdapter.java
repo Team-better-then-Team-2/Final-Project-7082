d2a6f4d38e49568debbf35c7a9ad938f997943f5
hughsong <hugh.happy.everyday@gmail.com>
2020-11-18 22:08:33 -0800

    Journal database Journal page update dialog

---
+++ app/src/main/java/com/example/final_project_7082/MainAdapter.java
@@ -0,0 +1,107 @@
+package com.example.final_project_7082;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
+import com.example.final_project_7082.Model.AppDatabase;
+import com.example.final_project_7082.Model.Journal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    List<Journal> journalList;
+    private Activity context;
+    private AppDatabase appDatabase;
+
+    public MainAdapter(Activity context, List<Journal> journalList){
+        this.context = context;
+        this.journalList = journalList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_row_main,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+            Journal data = journalList.get(position);
+
+        appDatabase = Room.databaseBuilder(context, AppDatabase.class,"word database")
+                .allowMainThreadQueries().build();
+        holder.textView.setText(data.getTitle());
+        holder.btEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Journal j = journalList.get(holder.getAdapterPosition());
+                 final int sID = j.getId();
+                 String title = j.getTitle();
+                 String content = j.getContent();
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.dialog_update);
+                int width = WindowManager.LayoutParams.MATCH_PARENT;
+                int height = WindowManager.LayoutParams.WRAP_CONTENT;
+                dialog.getWindow().setLayout(width,height);
+                dialog.show();
+
+                final EditText editTitle = dialog.findViewById(R.id.edit_title);
+                final EditText editContent = dialog.findViewById(R.id.edit_content);
+                Button save = dialog.findViewById(R.id.bt_update);
+
+                editTitle.setText(title);
+                editContent.setText(content);
+
+                save.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                       dialog.dismiss();
+
+                        String tmp1 = editTitle.getText().toString().trim();
+                        String tmp2 = editContent.getText().toString().trim();
+                        appDatabase.getJournalDao().update(sID,tmp1,tmp2);
+                        journalList.clear();
+                        journalList.addAll(appDatabase.getJournalDao().getAllJournal());
+                        notifyDataSetChanged();
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return journalList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        ImageView btEdit,btDelete;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text_view);
+            btEdit = itemView.findViewById(R.id.bt_edit);
+            btDelete = itemView.findViewById(R.id.bt_delete);
+        }
+    }
+}
--- app/src/main/java/com/example/final_project_7082/SecondFragment.java
+++ app/src/main/java/com/example/final_project_7082/SecondFragment.java
@@ -1,15 +1,38 @@
 package com.example.final_project_7082;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
+import com.example.final_project_7082.Model.AppDatabase;
+import com.example.final_project_7082.Model.Journal;
+import com.example.final_project_7082.Model.JournalDao;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondFragment extends Fragment {
+
+    EditText editText;
+    Button btAdd,btReset;
+    RecyclerView recyclerView;
+    List<Journal> journalList = new ArrayList<>();
+    LinearLayoutManager linearLayoutManager;
+    MainAdapter adapter;
+    AppDatabase appDatabase;
+    JournalDao journalDao;
 
     @Override
     public View onCreateView(
@@ -17,12 +40,13 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+/*
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,5 +54,43 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+ */
+        appDatabase = Room.databaseBuilder(view.getContext(), AppDatabase.class,"word database")
+                .allowMainThreadQueries().build();
+
+        journalDao=appDatabase.getJournalDao();
+        editText = view.findViewById(R.id.edit_text);
+        btAdd = view.findViewById(R.id.bt_add);
+        btReset = view.findViewById(R.id.bt_reset);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        journalList = journalDao.getAllJournal();
+
+        linearLayoutManager = new LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        //maybe problem
+        adapter = new MainAdapter((Activity) view.getContext(),journalList);
+        recyclerView.setAdapter(adapter);
+
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sTitle = editText.getText().toString().trim();
+
+                if(!sTitle.equals("")){
+                    Journal data = new Journal(sTitle,"today","haha");
+                    journalDao.addJournal(data);
+                    editText.setText("");
+                    journalList.clear();
+                    journalList.addAll(journalDao.getAllJournal());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+
+
+
+
     }
 }
